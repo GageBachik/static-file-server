3c528007980ceb2798caf1ab47f7e09431038ec7
gbachik <gbachik@gmail.com>
2014-07-22 12:26:11 -0600

    read a file

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
 var express = require('express');
-
 var app = express();
+
 
 app.get('/', function(req, res) {
 	res.send('<h1>Hello Boulder</h1>');
+});
+
+app.get('/data', function(req, res) {
+	var fileContents = fs.readFileSync('data.txt');
+	res.header('Content-Type', 'text/html');
+	res.send(fileContents);
 });
 
 var server = app.listen(5445, function() {
